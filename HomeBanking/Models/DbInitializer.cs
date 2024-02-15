cccd75403b4194da1c7ecd3af732bb6d31e9bc75
Matias_Pereda <Matias_Pereda@ITRP1112>
2024-02-15 15:00:31 -0300

    Add project files.

---
+++ HomeBanking/Models/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeBanking.Models
+{
+    public class DbInitializer 
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"}
+                };
+
+                context.Clients.AddRange(clients);
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
